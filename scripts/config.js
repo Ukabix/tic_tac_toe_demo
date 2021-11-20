d38c8753c3af72917384daa24b7cdebbf47ed787
Ukabix <54454097+Ukabix@users.noreply.github.com>
2021-11-20 18:26:41 +0100

    add form data js capture and whitespace validation

--- scripts/config.js
+++ scripts/config.js
@@ -6,4 +6,19 @@ function openPlayerConfig() {
 function closePlayerConfig() {
   playerConfigOverlayElement.style.display = "none";
   backdropElement.style.display = "none";
+  formElement.firstElementChild.classList.remove("error");
+  errorsOutputElement.textContent = "";
+}
+
+function savePlayerConfig(event) {
+  event.preventDefault(); // prevent default -> won't send http request
+  const formData = new FormData(event.target); // create an instance from blueprint
+  const enteredPlayername = formData.get("playername").trim(); // trim excess whitespace - validate spaces
+  //console.log(enteredPlayername);
+  if (!enteredPlayername) { // falsy! enteredPlayerName === ""
+    event.target.firstElementChild.classList.add("error");
+    errorsOutputElement.textContent = "Please enter a valid name";
+    return; // stops the function
+  }
+  
 }
